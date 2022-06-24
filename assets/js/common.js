a21423df6c1ee2119f0386c1bab1ede021e6be30
yoonzeen <yjieuno_o@naver.com>
2022-06-25 00:01:59 +0900

    Updates

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,8 @@
-function CalculateWidth() {
-    const screens = document.getElementsByClassName('screen');
-    const screensArray = Array.from(screens);
-    screensArray.forEach((el) => el.style.width = window.innerWidth + 'px');
-
-    const screenWrap = document.querySelector('.screen-wrap');
-    screenWrap.style.width = screensArray[0].clientWidth * screensArray.length + 'px';
-}
 setTimeout(() => {
 const loading = document.querySelector('.first-loading');
 loading.style.display = 'none';
 },2000);
 
-setTimeout(() => {
-    CalculateWidth();
-}, 1000);
 
 
 
